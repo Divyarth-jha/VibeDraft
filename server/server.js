731ad4e2eacc6bf0e3da7a65d7322efe181d4405
Divyarth-jha <divyarthjha@gmail.com>
2025-11-27 05:01:47 +0530

    fixing server

--- server/server.js
+++ server/server.js
@@ -1,35 +1,26 @@
-import express from 'express'
-import 'dotenv/config'
-import cors from 'cors'
+import express from 'express';
+import 'dotenv/config';
+import cors from 'cors';
 import ConnectDB from './configs/db.js';
 import adminRouter from './routes/adminRoutes.js';
 import blogRouter from './routes/blogRoutes.js';
 
 const app = express();
 
-await ConnectDB()
-
-// CORS config
-const allowedOrigins = [
-  'https://vibe-draft.vercel.app',
-  'http://localhost:5173'
-];
+await ConnectDB();
 
 app.use(cors({
-  origin: allowedOrigins,
-  credentials: true,
+  origin: "https://vibe-draft-seven.vercel.app",
+  credentials: true
 }));
 
-app.use(express.json())
+app.options('*', cors());
+app.use(express.json());
 
 // Routes
-app.get('/', (req, res) => res.send("API is Working"))
-app.use('/api/admin', adminRouter)
-app.use('/api/blog', blogRouter)
+app.get('/', (req,res)=>res.send("API is Working"));
+app.use('/api/admin', adminRouter);
+app.use('/api/blog', blogRouter);
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log('Server is running on PORT ' + PORT);
-})
-
-export default app;
+app.listen(PORT, () => console.log("Server running on PORT " + PORT));
